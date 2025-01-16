4dc04c262a6875389253021d577c4f08691ab5d8
emsmoraes <eduardomoraesdeveloper@gmail.com>
2025-01-16 02:13:06 -0300

    feat: init pages transition indicator

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "./_providers/theme-provider";
 import Navbar from "./_components/Navbar";
 import { Toaster } from "./_components/ui/sonner";
 import Footer from "./_components/Footer";
+import NextTopLoader from "nextjs-toploader";
 
 export const metadata: Metadata = {
   title: "Chaxis",
@@ -21,6 +22,16 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="light">
           <div className={`pb-16 antialiased`}>
             <Navbar />
+            <NextTopLoader
+              color="#2299DD"
+              initialPosition={0.08}
+              crawlSpeed={200}
+              height={3}
+              crawl={false}
+              showSpinner={false}
+              easing="ease"
+              speed={200}
+            />
             {children}
           </div>
           <Footer />
